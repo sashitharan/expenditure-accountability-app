4b0aec7b00e72b456331d02633bbd5bf3de8cf1c
Sashitharan <rs003@mymail.sim.edu.sg>
2021-06-06 11:18:09 +0800

    Adding Header Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from "react";
+import {Header} from './components/Header';
 function App() {
   return (
     <div>
-      Hello world
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+//rafc = fucntional component
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <h2>
+            Expense Tracker
+        </h2>
+    )
+}
