55f5a647315c3adbb6f4ffc9b1dde62490f33203
Sashitharan <rs003@mymail.sim.edu.sg>
2021-06-10 14:04:44 +0800

    Settled the mathematical signs!

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Header} from './components/Header';
 import {Balance} from './components/Balance';
 import {IncomeExpenses} from './components/IncomeExpenses';
-import {Transactions} from './components/Transactions';
+import {Transactions} from './components/TransactionsList';
 import {AddTransaction} from './components/AddTransaction';
 import './App.css';
 
