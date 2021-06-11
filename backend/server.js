e896708c976cbdf319b7f8f58f45783e894530da
Sashitharan <rs003@mymail.sim.edu.sg>
2021-06-11 16:33:55 +0800

    First version, API

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+
 dotenv.config({path: './config/config.env'});
 
+const transaction = require('./routes/transaction');
 const app = express();
 
-app.get('/', (req,res)=> res.send('Hello'));
+// app.get('/', (req,res)=> res.send('Hello'));
 
+app.use('/api/v1/transaction', transaction);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT), console.log(`Server running in ${process.env.NODE_ENV} mode and in port ${PORT}`.yellow.bold);
+app.listen(PORT), console.log(`Server running in ${process.env.NODE_ENV} mode and in port ${PORT}`.blue.bold);
