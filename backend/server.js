aa1b645a1158e1fe45b73b8939da3de119d93d63
Sashitharan <rs003@mymail.sim.edu.sg>
2021-06-11 16:24:59 +0800

    Integrating backend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+dotenv.config({path: './config/config.env'});
+
+const app = express();
+
+app.get('/', (req,res)=> res.send('Hello'));
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT), console.log(`Server running in ${process.env.NODE_ENV} mode and in port ${PORT}`.yellow.bold);
