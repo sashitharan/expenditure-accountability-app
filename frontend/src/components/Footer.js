cd8f4bda4249bb9ac1cbd68c6985886ff462a0df
Sashitharan <rs003@mymail.sim.edu.sg>
2021-06-11 19:44:45 +0800

    make it better

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-       <div> 
+       <div className="wrapper row5"> 
 Designed by: <a href="https://www.linkedin.com/in/sashitharan/" target="_blank"> Sashitharan</a> 
        </div>
         
